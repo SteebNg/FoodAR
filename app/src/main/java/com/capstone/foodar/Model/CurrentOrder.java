c624ebece649ae3c4ed06dcde2db78bc8e261060
NgWeiCheng <92345739+SteebNg@users.noreply.github.com>
2025-09-22 22:42:17 +0800

    Added timestamp for currentOrder

--- app/src/main/java/com/capstone/foodar/Model/CurrentOrder.java
+++ app/src/main/java/com/capstone/foodar/Model/CurrentOrder.java
@@ -1,9 +1,12 @@
 package com.capstone.foodar.Model;
 
+import com.google.firebase.Timestamp;
+
 import java.util.ArrayList;
 
 public class CurrentOrder {
     public String locationId, userId, paymentMethod, servingMethod, tableNum, destination, currentOrderId, status;
     public ArrayList<FoodInCart> foods;
     public double orderTotalPrice;
+    private Timestamp timestamp;
 }
