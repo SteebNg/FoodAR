813c77e1f97dadefd5cf31d16babafdcf0ea56c0
NgWeiCheng <92345739+SteebNg@users.noreply.github.com>
2025-10-17 16:48:35 +0800

    Fixed crash by converting Firebase Timestamp to Java native timestamp

--- app/src/main/java/com/capstone/foodar/Adapter/OrderHistoryParentRecyclerListAdapter.java
+++ app/src/main/java/com/capstone/foodar/Adapter/OrderHistoryParentRecyclerListAdapter.java
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.capstone.foodar.Model.OrderHistoryFoodParent;
 import com.capstone.foodar.R;
 
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Locale;
@@ -29,7 +30,7 @@ public class OrderHistoryParentRecyclerListAdapter extends RecyclerView.Adapter<
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.layout_food_details_review_item_list,
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_order_history_item_list,
                 parent,
                 false);
 
@@ -40,7 +41,7 @@ public class OrderHistoryParentRecyclerListAdapter extends RecyclerView.Adapter<
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         OrderHistoryFoodParent foodsOnThatDate = orderHistoryFoodParents.get(position);
         SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM", Locale.getDefault());
-        holder.date.setText(sdf.format(foodsOnThatDate.timestamp));
+        holder.date.setText(sdf.format(firebaseToJavaUtilTimestamp(foodsOnThatDate.timestamp)));
 
         OrderHistoryChildRecyclerListAdapter adapter = new OrderHistoryChildRecyclerListAdapter(orderHistoryFoodParents.get(position).foodsInCart, context);
         holder.recyclerIndiFoodsHistory.setAdapter(adapter);
@@ -51,6 +52,20 @@ public class OrderHistoryParentRecyclerListAdapter extends RecyclerView.Adapter<
     public int getItemCount() {
         return orderHistoryFoodParents.size();
     }
+
+    private Timestamp firebaseToJavaUtilTimestamp(com.google.firebase.Timestamp firebaseTimestamp) {
+        long seconds = firebaseTimestamp.getSeconds();
+        int nanos = firebaseTimestamp.getNanoseconds();
+
+        long milliseconds = seconds * 1000;
+
+        Timestamp javaTimestamp = new Timestamp(milliseconds);
+
+        javaTimestamp.setNanos(nanos);
+
+        return javaTimestamp;
+    }
+
 
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView date;
